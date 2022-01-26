17c9572ea13befd4c6598be7652878cb98c21d64
jiang-30 <514770300@qq.com>
2022-01-26 18:31:26 +0800

    f

---
+++ combo-platform/src/main/java/org/jiang/combo/platform/config/ResponseAdvice.java
@@ -0,0 +1,50 @@
+package org.jiang.combo.platform.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.jiang.combo.platform.dto.Result;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class ResponseAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @SneakyThrows
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if(body instanceof String) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return  objectMapper.writeValueAsString(Result.success(body));
+        }
+
+        if(body instanceof  Result) {
+            return  body;
+        }
+
+        return Result.success(body);
+    }
+
+    /**
+     * 全局统一异常处理
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result<String> exception(Exception e) {
+        System.out.println(e);
+        return Result.fail(e);
+    }
+
+}
