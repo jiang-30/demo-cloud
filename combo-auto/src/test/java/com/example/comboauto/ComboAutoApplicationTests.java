62c9e9b394c4334fd3b53a494b55100fbdde8666
jiang-30 <514770300@qq.com>
2022-04-06 09:13:02 +0800

    f

---
+++ combo-auto/src/test/java/com/example/comboauto/ComboAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comboauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComboAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
