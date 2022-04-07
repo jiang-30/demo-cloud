ca4ee74443f590bccb25b05a1fe2ca9191234457
jiang <jiang_30@aliyun.com>
2022-04-07 08:11:30 +0800

    f

---
+++ combo-admin/src/main/java/org/jiang/combo/admin/config/AppProperties.java
@@ -0,0 +1,10 @@
+package org.jiang.combo.admin.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+//@ConfigurationProperties(prefix = "combo")
+public class AppProperties {
+    private String url;
+}
